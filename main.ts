7accd1f06f2b531d4fd17fddab8ca8a92927e9c3
Jakob <84283522+Mee632@users.noreply.github.com>
2025-04-11 10:24:31 +0200

    init

--- main.ts
+++ main.ts
@@ -1,134 +1,121 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Console } from 'console';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface WordsTrackerSettings {
+    streak: number;
+    lastCheckedDate: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: WordsTrackerSettings = {
+    streak: 0,
+    lastCheckedDate: ''
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class WordsTracker extends Plugin {
+    settings: WordsTrackerSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // This creates an icon in the left ribbon.
+        const ribbonIconEl = this.addRibbonIcon('flame', '750 Words Challenge', async () => {
+            await this.checkDailyWordCount();
+            new Notice(`Your current streak is ${this.settings.streak} days!`);
+        });
+        ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // This adds a status bar item to the bottom of the app.
+        const statusBarItemEl = this.addStatusBarItem();
+        statusBarItemEl.setText(`Streak: ${this.settings.streak} days`);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        // Update streak daily when the plugin is loaded
+        await this.checkDailyWordCount();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new WordsTrackerSettingTab(this.app, this));
+    }
+
+    onunload() {
+        // Cleanup if needed
+    }
+
+    async checkDailyWordCount() {
+		const today = this.getTodayDate();
+		if (this.settings.lastCheckedDate === today) {
+			return; // Already checked today
+		}
+	
+		// Construct the full path to the daily file
+		const dailyFileName = `${today}.md`;
+		const dailyFilePath = `/Users/jakobwimmer/Desktop/TestForPlugin/${dailyFileName}`;
+		const dailyFile = this.app.vault.getAbstractFileByPath(dailyFilePath);
+	
+		if (dailyFile instanceof TFile) {
+			const fileContent = await this.app.vault.read(dailyFile);
+			const wordCount = this.getWordCount(fileContent);
+	
+			if (wordCount >= 750) {
+				if (this.settings.lastCheckedDate !== today) {
+					this.settings.streak += 1;
 				}
+			} else {
+				this.settings.streak = 0; // Reset streak if less than 750 words
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		} else {
+			this.settings.streak = 0; // Reset streak if the file doesn't exist
+		}
+	
+		this.settings.lastCheckedDate = today;
+		await this.saveSettings();
 	}
 
-	onunload() {
-
+    getTodayDate(): string {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+		console.log(`Today's date: ${this.getTodayDate}`);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    getWordCount(content: string): number {
+        return content.split(/\s+/).filter(word => word.length > 0).length;
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class WordsTrackerSettingTab extends PluginSettingTab {
+    plugin: WordsTracker;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: WordsTracker) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        new Setting(containerEl)
+            .setName('Current Streak')
+            .setDesc('Your current writing streak in days.')
+            .addText(text => text
+                .setValue(this.plugin.settings.streak.toString())
+                .setDisabled(true));
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Last Checked Date')
+            .setDesc('The last date the streak was checked.')
+            .addText(text => text
+                .setValue(this.plugin.settings.lastCheckedDate)
+                .setDisabled(true));
+    }
 }
